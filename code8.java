fb338e952368560fdc8949a6446595a982e9c053
mayurv06 <mayurv337@gmail.com>
2026-01-29 15:42:51 +0530

    Add files via upload

---
+++ code8.java
@@ -0,0 +1,9 @@
+class code8 {
+    public static void main(String[] args) {
+        float P=10.0f;
+        float R=15.0F;
+        float T=20.0f;
+        float SI=(P*R*T)/100;
+        System.out.println("SI="+SI);
+    }
+}
