2dfe5353828abbf4be6f786c526a3a46e460f9f5
mayurv06 <mayurv337@gmail.com>
2026-01-29 14:12:21 +0530

    Add files via upload

---
+++ code1.java
@@ -0,0 +1,14 @@
+class code1{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=b/a;
+        System.out.println("sum="+sum);
+        System.out.println("sub="+sub);
+        System.out.println("mul="+mul); 
+        System.out.println("div="+div);
+}
+}
---
+++ code3.java
@@ -0,0 +1,8 @@
+class code3{
+    public static void main(String[] args){
+        double a=10.0;
+        int b=(int)a;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
